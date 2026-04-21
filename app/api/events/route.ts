8b93f167ed4bc0a7dd85789c8904015fce70c736
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-04-21 15:14:30 +0000

    implement GET api route

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
 import Event from "@/database/event.model";
 import { NextRequest,NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
+import { v2 as cloudinary } from 'cloudinary';
+
 
 
 export async function POST(request: NextRequest) {
@@ -18,6 +20,24 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ message: 'Invalid form data', error: error instanceof Error ? error.message : 'Unknown error' }, { status: 400 });
         }
 
+        const file = forData.get('image') as File | null;
+
+        if (!file) {
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 });
+        }
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new  Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'events' }, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {                    resolve(result);
+                }            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
@@ -26,4 +46,17 @@ export async function POST(request: NextRequest) {
         console.error('Error creating event:', error);
         return NextResponse.json({ message: 'Failed to create event', error: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 });
     }   
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+        return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
+
+    } catch (error) {
+        console.error('Error fetching events:', error);
+        return NextResponse.json({ message: 'Failed to fetch events', error: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 });
+    }
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/df44y3eqt/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
